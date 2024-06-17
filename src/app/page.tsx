60e1b5c8cd1e3272e206c8e034a1c8cfe6dd9313
andrei <andrei.lazo.30@gmail.com>
2024-06-17 14:46:25 +0800

    commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,9 @@ import SoilMoistureHistory from '@/components/soil-moisture/SoilMoistureHistory'
 
 const getRecentMoistureData = async () => {
   const res = await fetch(
-    'https://api.thingspeak.com/channels/2562479/fields/1.json?results='
+    'https://api.thingspeak.com/channels/2562479/fields/1.json', {
+      cache: 'no-store'
+    }
   );
 
   if (!res.ok) {
